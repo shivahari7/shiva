a56d76ec3c12533dbe674be7cea724e3e111476b
shivahari7 <124660178+shivahari7@users.noreply.github.com>
2023-02-12 08:40:40 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6054706338:AAEvWNFwzascy0oMwcl8Pe2o1nlGO1kwLVU")
+    API_ID = int(os.environ["API_ID", 20937540]
+    API_HASH = os.environ["API_HASH", "bf35fcc80dcea68066ec08b588cde18d"]
+    AUTH_USERS = "2049857696 """
+
---
+++ config.py.bak
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5735888887:AAEqjab1kHPaQvvSaa8Qqv5yV_7l7AqcAwI")
+    API_ID = int(os.environ["API_ID", 1654363]
+    API_HASH = os.environ["API_HASH", "26b911420edb5ceb8f370f21f5eb2684"]
+    AUTH_USERS = "435946586 """
+
